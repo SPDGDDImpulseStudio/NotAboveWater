56ee26d66a6cdc73e8fc9ba5a9978df28a5521da
jahhzhengc <33411360+jahhzhengc@users.noreply.github.com>
2017-11-12 01:09:35 +0800

    Prototyping and scripts

---
+++ Assets/Resources/Scripts/Events/EventD.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Tools/EventD", fileName = "EventD_", order = 4)]
+public class EventD : EventsInterface {
+    public override void CurrEvent()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override IEnumerator EnumEvent()
+    {
+        Player.Instance.allowToShoot = true;
+        AI ai = FindObjectOfType<AI>();
+        while(ai.currHealth > 0 && Player.Instance.currHealth > 0)
+        {
+            yield return null;
+        }
+        Player.Instance.allowToShoot = false;
+        EndOfEvent();
+    }
+
+  
+}
