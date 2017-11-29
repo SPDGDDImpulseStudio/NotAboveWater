115e2da083e7a6f87393be00e4cebf2750668411
Anzen <weiloon1997@hotmail.com>
2017-11-29 17:46:27 +0800

    Update Main Menu,Credits and settings. I give up on the UI. Just do it for me JZ.

---
+++ Assets/Resources/Scenes/UI Art/MouseCursor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseCursor : MonoBehaviour
+{
+
+    public Texture2D defaultAimTexture;
+    public CursorMode curMode = CursorMode.Auto;
+    public Vector2 hotspot = Vector2.zero;
+
+    void Start()
+    {
+        
+        Cursor.SetCursor(defaultAimTexture, hotspot, curMode);
+    }
+
+}
