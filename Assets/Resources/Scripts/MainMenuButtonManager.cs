0bb9f02b38488358f475331bd362ee2ffb83f150
jahhzhengc <33411360+jahhzhengc@users.noreply.github.com>
2017-11-30 06:43:25 +0800

    Building setting

--- Assets/Resources/Scripts/MainMenuButtonManager.cs
+++ Assets/Resources/Scripts/MainMenuButtonManager.cs
@@ -10,7 +10,6 @@ public class MainMenuButtonManager : MonoBehaviour
     public void btn_NewGame(string Javan_Test)
     {
         SceneManager.LoadScene(Javan_Test);
-        Debug.Log("loaded");
     }
 
     public void btn_QuitGame()
@@ -21,24 +20,20 @@ public class MainMenuButtonManager : MonoBehaviour
     public void btn_Settings(string settingsMenu)
     {
         SceneManager.LoadScene(settingsMenu);
-        Debug.Log("LUL");
     }
 
     public void btn_Credits(string creditsMenu)
     {
         SceneManager.LoadScene(creditsMenu);
-        Debug.Log("Kappa");
     }
 
     public void btn_CreditsBack(string menu)
     {
         SceneManager.LoadScene(menu);
-        Debug.Log("hAhAA");
     }
 
     public void btn_SettingsBack(string menu)
     {
         SceneManager.LoadScene(menu);
-        Debug.Log("I'm 12 btw");
     }
 }
