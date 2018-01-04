f07b9a61e256f822d186fef67f16cc55ee73201d
Aries Kee <arieskee23@gmail.com>
2018-01-04 14:19:17 +0800

    Created Audio Cross-fade system

---
+++ Assets/AudioColliderSnapshot.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioColliderSnapshot : MonoBehaviour {
+
+    public AudioMixerSnapshot InsideColliderSnap;
+    public AudioMixerSnapshot OutsideColliderSnap;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        InsideColliderSnap.TransitionTo(0.5f);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        OutsideColliderSnap.TransitionTo(0.5f);
+    }
+
+
+}
