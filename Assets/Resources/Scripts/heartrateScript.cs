ee902fd27ee14d2fa945dcdc3a3d60b74a302417
ImpulseStudioSPDGDD <33388481+ImpulseStudioSPDGDD@users.noreply.github.com>
2018-01-15 16:56:18 +0800

    - Extra Heartrate UI

--- Assets/Resources/Scripts/heartrateScript.cs
+++ Assets/Resources/Scripts/heartrateScript.cs
@@ -6,6 +6,8 @@ using UnityEngine.UI;
 public class heartrateScript : MonoBehaviour
 {
     public Slider heartRate;
+    public Image spr_heartRate;
+    public List<Sprite> images;
 
     public void Update()
     {
@@ -13,5 +15,12 @@ public class heartrateScript : MonoBehaviour
             heartRate.value += 0.1f * Time.deltaTime;
         else if (heartRate.value == 1)
             heartRate.value = 0;
+
+        if (Player.Instance.currHealth > Player.Instance.maxHealth / 2)
+            spr_heartRate.sprite = images[0];
+        else if (Player.Instance.currHealth > Player.Instance.maxHealth / 4 && Player.Instance.currHealth <= Player.Instance.maxHealth / 2)
+            spr_heartRate.sprite = images[1];
+        else if (Player.Instance.currHealth <= Player.Instance.maxHealth / 4)
+            spr_heartRate.sprite = images[2];
     }
 }
