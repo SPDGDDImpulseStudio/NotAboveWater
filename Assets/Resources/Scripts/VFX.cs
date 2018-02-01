30e7cd193e1f59b66d40616cc1873a24a54d1476
jahhzhengc <33411360+jahhzhengc@users.noreply.github.com>
2018-02-01 10:45:31 +0800

    vfx cs

---
+++ Assets/Resources/Scripts/VFX.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VFX : PoolObject {
+    ParticleSystem pSystem;
+    void Start()
+    {
+        if (!pSystem)
+            pSystem = GetComponentInChildren<ParticleSystem>();
+
+    }
+    public override void Init()
+    {
+        this.gameObject.SetActive(true);
+        if(!pSystem)
+        pSystem = GetComponentInChildren<ParticleSystem>();
+
+
+        pSystem.Stop();
+        pSystem.Play();
+    }
+
+    public override void TurnOff()
+    {
+        //if (!pSystem)
+        //    pSystem = GetComponentInChildren<ParticleSystem>();
+        if(pSystem)
+        pSystem.Stop();
+
+        this.gameObject.SetActive(false);
+
+    }
+
+    void RepositionSelf()
+    {
+
+    }
+}
