cf120cf173fb5d7bd800cbfbcbdfd5e9f61b6e81
jahhzhengc <33411360+jahhzhengc@users.noreply.github.com>
2018-02-08 07:39:50 +0800

    boss.cs update

--- Assets/Resources/Scripts/Boss.cs
+++ Assets/Resources/Scripts/Boss.cs
@@ -9,38 +9,66 @@ public class Boss : MonoBehaviour {
     List<Tentacle> tentacles = new List<Tentacle>();
     float timer = 3f, timerNow = 0f;
 
+    public List<AudioClip> sfxBoss = new List<AudioClip>();
+    public AudioSource aSource;
+
     void Start () {
         bossCurrHealth = bossMaxHealth;
         tentacles = new List<Tentacle>(FindObjectsOfType<Tentacle>());
-	}
+        if (!aSource)
+            aSource = GetComponent<AudioSource>();
+        StartCoroutine(HealthChecker());
+        StartCoroutine(TriggerTen());
+    }
     public UnityEngine.UI.Slider slider;
-
-    public void ShootOnShell()
+    IEnumerator HealthChecker()
     {
-
-    }
-
-    public void ShootOnCore()
-    {
-
-    }
-	
-	// Update is called once per frame
-	void Update () {
-        //slider.value = (bossCurrHealth / bossMaxHealth);
-        if (timerNow < timer) timerNow += Time.deltaTime;
-        else
+        while(bossCurrHealth > 0)
         {
-            int b = (int)Random.Range(0, tentacles.Count );
-            for (int i = 0; i < tentacles.Count; i++)
-            {
-                if (i == b) tentacles[i].selectOne = true;
-                else
-                    tentacles[i].selectOne = false;
-            }
-            timer = Random.Range(3f, 4f);
-            timerNow = 0f;
+            //slider.value = (bossCurrHealth / bossMaxHealth);
+
+            yield return null;
         }
-	}
+        StartCoroutine(BossDie());
+    }
+
+    IEnumerator TriggerTen()
+    {
+        while (true)
+        {
+            if (timerNow < timer) timerNow += Time.deltaTime;
+            else
+            {
+                int b = (int)Random.Range(0, tentacles.Count);
+                for (int i = 0; i < tentacles.Count; i++)
+                {
+                    if (i == b) tentacles[i].selectOne = true;
+                    else
+                        tentacles[i].selectOne = false;
+                }
+                timer = Random.Range(3f, 4f);
+                timerNow = 0f;
+            }
+            yield return null;
+        }
+    }
+    IEnumerator BossDie()
+    {
+        //yield return new WaitUntil(()=> )
+        yield return null;
+        Debug.Log("Boss DIe");
+        SceneChanger.Instance.Fading(0);
+    }
+
+    public void OnHit()
+    {
+        if (!aSource.isPlaying)
+        {
+            int x = (int)Random.Range(0, sfxBoss.Count);
+            aSource.clip = sfxBoss[x];
+            aSource.Play();
+        }
+        bossCurrHealth -= Random.Range(15f, 30f);
+    }
 
 }
