895b45fbc2369d3adba7be955290f016121259a9
jahhzhengc <33411360+jahhzhengc@users.noreply.github.com>
2018-02-22 21:05:15 +0800

    no message

--- Assets/Resources/Scripts/FishScript.cs
+++ Assets/Resources/Scripts/FishScript.cs
@@ -1,24 +1,28 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FishScript : PoolObject {
+public class FishScript : MonoBehaviour, PoolObject {
     public float fishSpeed = 25f;   
 	// Use this for initialization
 	void Start () {
 		
 	}
 	
-    public override void Init()
-    {
-
-    }
-    public override void TurnOff()
-    {
-
-    }
+ 
 	// Update is called once per frame
 	void FixedUpdate () {
         this.transform.position = Vector3.MoveTowards(this.transform.position, Player.Instance.transform.position, fishSpeed* Time.deltaTime);
 	}
+
+    public void Init()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void TurnOff()
+    {
+        throw new NotImplementedException();
+    }
 }
