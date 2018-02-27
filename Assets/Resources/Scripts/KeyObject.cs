c1ab5ea6ab417c0883be8427d43a34e86a69f1b7
DGDD-NB1543685\Javan <teongks821@gmail.com>
2018-02-28 05:51:59 +0800

    key objects script

---
+++ Assets/Resources/Scripts/KeyObject.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyObject : MonoBehaviour {
+
+    public Collider col;
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    public CirclePosUpdate circle;
+    public void DeductCircleHealth()
+    {
+        if (!circle) return;
+        circle.health--;
+        if (circle.health < 1)
+        {
+            Debug.Log(circle.health);
+            TurnOff();
+        }
+    }
+
+    void TurnOff()
+    {
+        Time.timeScale = 1f;
+        if (circle != null)
+            circle.TurnOff();
+        gameObject.SetActive(false);
+        Player.Instance.GainScore(UnityEngine.Random.Range(95, 105));
+
+    }
+}
